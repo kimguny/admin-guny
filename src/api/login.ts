4f484bb64f4f9e362cb19185d49dbe0e884d2a80
kimguny <kimguny12@gmail.com>
2025-01-31 14:40:48 +0900

    ✨ login.ts 추가

---
+++ src/api/login.ts
@@ -0,0 +1,9 @@
+import { APIResponse } from "@/types/login";
+import axios from "axios";
+
+const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+export const login = async (data: { user_id: string; password: string }) => {
+  const res = await axios.post<APIResponse>(`${apiBaseUrl}/api/login`, data);
+  return res.data;
+};
