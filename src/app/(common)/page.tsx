ad9c28e1cc6d6137c4ccfc85840857f8b4895b2c
kimguny <kimguny12@gmail.com>
2025-01-31 14:43:41 +0900

    🎨 페이지 분리

---
+++ src/app/(common)/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useCookies } from "react-cookie";
+import React, { useEffect } from "react";
+
+export default function Page() {
+  const [cookies] = useCookies();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (cookies.accessToken) {
+      router.push("/");
+    } else {
+      router.push("/login");
+    }
+  }, [cookies.accessToken, router]);
+
+  return <></>;
+}
