5d255f145de1444ac7dafe554d178961eda417c5
kimguny <kimguny12@gmail.com>
2025-01-31 14:42:06 +0900

    ✨ login 뮤테이션 추가

---
+++ src/hooks/mutation/useLoginMutation.tsx
@@ -0,0 +1,31 @@
+import { setCookie } from "@/utils/cookies";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { login } from "@/api/login";
+import { AxiosError } from "axios";
+
+export default function useLoginMutation() {
+  const queryClient = useQueryClient();
+  const router = useRouter();
+
+  const mutate = useMutation({
+    mutationFn: login,
+    onSuccess: (data) => {
+      setCookie("accessToken", data.accessToken, { path: "/" });
+      localStorage.setItem("refreshToken", data.refreshToken);
+
+      setCookie("loginType", "normal", { path: "/" });
+
+      alert(data.message);
+
+      queryClient.invalidateQueries({ queryKey: ["user"] });
+
+      router.push("/");
+    },
+    onError: (error: AxiosError) => {
+      const errorMessage = (error.response?.data as { error: string }).error;
+      alert(errorMessage);
+    },
+  });
+  return mutate;
+}
